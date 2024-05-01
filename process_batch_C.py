126c441c68ea02cebdd537897f96ecf0bac296df
LuciMcD <lucimcdaniel@gmail.com>
2024-04-30 21:51:16 -0500

    finishing batch_c

--- process_batch_C.py
+++ process_batch_C.py
@@ -34,11 +34,35 @@ OUTPUT_FILE_NAME = "batchfile_3_farenheit.csv"
 
 
 def convert_k_to_f(temp_k):
-    return
+    logging.debug(f"Calling convert_k_to_f() with {temp_k}")
+    fahrenheit = round(1.8*(float(temp_k) - 273), 2)
+    logging.debug(f"Converted {temp_k}K to {fahrenheit}F.")
+    return fahrenheit
 
 
 def process_rows(input_file_name, output_file_name):
-    return
+    logging.info(f"Calling process_rows(): {input_file_name} to {output_file_name}.")
+    
+    with open(input_file_name, "r") as input_file:
+        logging.info(f"Opened for reading: {input_file_name}.")
+        reader = csv.reader(input_file, delimiter=",")
+
+        header = next(reader)
+        logging.info(f"Skipped header row: {header}")
+
+        with open(output_file_name, "w", newline="") as output_file:
+            logging.info(f"Opened for writing:{output_file_name}.")
+            writer = csv.writer(output_file, delimiter=",")
+
+            writer.writerow(["Year", "Month", "Day", "Time", "TempF"])
+
+            for row in reader:
+                Year, Month, Day, Time, TempK = row
+
+                TempF = convert_k_to_f(TempK)
+
+                writer.writerow([Year, Month, Day, Time, TempF])
+    
 
 
 # ---------------------------------------------------------------------------
